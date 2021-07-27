090d3356de38d1e957ad1626a52df5690ed7c90b
qcSecret <mango.kamchatka.1997@gmail.com>
2021-07-27 21:01:34 +1200

    Cleanup fixes

--- src/Traits/HasAnchorBinding.php
+++ src/Traits/HasAnchorBinding.php
@@ -4,9 +4,7 @@ namespace Cryptstick\Performs\Traits;
 
 use Cryptstick\Performs\Exceptions\BasePerformException;
 use Cryptstick\Performs\PerformsFacade;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
-use Throwable;
 
 trait HasAnchorBinding
 {
